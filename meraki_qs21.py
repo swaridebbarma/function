71d45705e932db7d031529d5ada2c967a713488b
swaridebbarma <98303710+swaridebbarma@users.noreply.github.com>
2022-08-29 23:22:14 +0530

    Add files via upload

---
+++ meraki_qs21.py
@@ -0,0 +1,9 @@
+#MERAKI.Q21
+def first_function():
+    s = 'I love India'
+    def second_function():
+        s = "MY NAME IS SWARI"
+        print(s)  
+    second_function()
+    print(s) 
+first_function()
