6d0c5ffee0b89e68dee2097094eab76dfda03ea4
swaridebbarma <98303710+swaridebbarma@users.noreply.github.com>
2022-08-29 23:33:04 +0530

    Add files via upload

---
+++ meraki_qs9.py
@@ -0,0 +1,7 @@
+#meraki.q9
+def function_print_lines ():
+    def function_print():
+        print ("mera name Rishabh hai.")
+    function_print()
+    print ("main NavGurukul ka co-Founder hun.")
+function_print_lines ()
