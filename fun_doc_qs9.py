c8ab5903fcf5ce7a49e0fe2ae0fd87c146b8868c
swaridebbarma <98303710+swaridebbarma@users.noreply.github.com>
2022-08-29 22:47:09 +0530

    Add files via upload

---
+++ fun_doc_qs9.py
@@ -0,0 +1,16 @@
+#doc.q9
+def squar_number():
+    i=1
+    a=[]
+    while i <= 5:
+        m=i**2
+        a.append(m)
+        i+=1
+    i=25
+    b=[]
+    while i <= 30:
+        m=i**2
+        b.append(m)
+        i+=1
+    print(a,",",b)
+squar_number()
