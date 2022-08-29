02e1c904ac09cfc09d41bb555f25b5f7f651145e
swaridebbarma <98303710+swaridebbarma@users.noreply.github.com>
2022-08-29 23:10:19 +0530

    Add files via upload

---
+++ more_qs2.py
@@ -0,0 +1,9 @@
+def fun (student,kharcha):
+    if kharcha<=50000:
+        print("hum budget ke andar hai")
+    else:
+        print("hum budget ki bahar hai")
+student=int(input("enter"))
+kharcha=int(input("enter"))
+fun(student,kharcha)
+#more_qs2
