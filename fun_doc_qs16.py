00a7d086a315a74010e0c6b7a1911866e42e78ce
swaridebbarma <98303710+swaridebbarma@users.noreply.github.com>
2022-08-29 22:42:34 +0530

    Add files via upload

---
+++ fun_doc_qs16.py
@@ -0,0 +1,19 @@
+#doc.q16
+def fun():
+    i=1
+    while i<=12:
+        a=i*n
+        print(i,"*",n,"=",a)
+        i=i+1
+n= int(input('enter no'))
+fun()
+        
+# def fun():    
+#     n= int(input('enter no'))
+#     n2= int(input('enter no'))
+#     i=1
+#     while i<=n2:
+#        a=i*n
+#        print(n,"*",i,"=",a)
+#        i=i+1
+# fun()
