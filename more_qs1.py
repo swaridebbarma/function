a225d054294e784893afc2b45235cd9f15154a55
swaridebbarma <98303710+swaridebbarma@users.noreply.github.com>
2022-08-29 23:08:57 +0530

    Add files via upload

---
+++ more_qs1.py
@@ -0,0 +1,13 @@
+def fun():
+    i=1
+    while i<=1000:
+        if i%3==0:
+            print("nav:",i)
+        if i%7==0:
+            print("gurukul:",i)
+        if i%21==0:
+            print("navgurukul:",i)
+        i+=1
+fun()
+#more_qs1
+    
