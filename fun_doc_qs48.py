448edbfb69df105f84ef402f8b58b1c20bae9ec7
swaridebbarma <98303710+swaridebbarma@users.noreply.github.com>
2022-08-29 22:15:03 +0530

    Add files via upload

---
+++ fun_doc_qs48.py
@@ -0,0 +1,13 @@
+#doc.q48
+def function(num,power):
+    i=0
+    while i<=num:
+        m=num**power
+        i=i+1
+    print(m)
+num=int(input("enter no:-"))
+power=int(input("enter no:-"))
+function(num,power)
+
+
+        
