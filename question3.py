0ada6f279090949a574e8c905b24439439c9a2c4
swaridebbarma <98303710+swaridebbarma@users.noreply.github.com>
2022-08-29 21:57:52 +0530

    Add files via upload

---
+++ question3.py
@@ -0,0 +1,10 @@
+#Q3
+def function():
+    sum=a+b+c
+    print(sum)
+    avg=sum//3
+    print(avg)   
+a=int(input("enter any no"))
+b=int(input("enter any no"))
+c=int(input("enter any no"))    
+function()
