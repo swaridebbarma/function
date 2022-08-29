e0de8fc1764b517e0e9225e3c052a6f80c06d2c2
swaridebbarma <98303710+swaridebbarma@users.noreply.github.com>
2022-08-29 23:37:24 +0530

    Add files via upload

---
+++ meraki_qs3.py
@@ -0,0 +1,10 @@
+#meraki.q3
+def function_say_bye():
+    print("Aapko mil ke maza aaya. ")
+    print("Bye bye")
+function_say_bye()
+function_say_bye()
+print("Python ka istamaal bahot jagah hota hai.")
+function_say_bye()
+function_say_bye()
+
