6f8c6576943fde3a3104f5ada8988be194130d00
swaridebbarma <98303710+swaridebbarma@users.noreply.github.com>
2022-08-29 22:31:26 +0530

    Add files via upload

---
+++ fun_doc_qs31.py
@@ -0,0 +1,10 @@
+#doc.q31
+def multi_table(number):
+    i=1
+    while i<=10:
+        m=number*i
+        print(i,'*',number,'=',m)
+        i=i+1
+# number=int(input("enter number:-"))
+multi_table(5)
+        
