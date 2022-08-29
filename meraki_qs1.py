6a546726af0363a68aab19e8ea23b7dc43fd12e8
swaridebbarma <98303710+swaridebbarma@users.noreply.github.com>
2022-08-29 23:37:54 +0530

    Add files via upload

---
+++ meraki_qs1.py
@@ -0,0 +1,14 @@
+#meraki.q1
+def fun():
+  n=[12,23,34,46,11]
+  i=0
+  min=0
+  while i<len(n):
+    if min<n[i]:
+      min=n[i]
+    elif min<n[i]:
+      min=n[i]
+    i=i+1
+  print(min)
+fun()
+
