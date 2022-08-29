88fa9ccb94d6f521266f6a89f34382757a32fe13
swaridebbarma <98303710+swaridebbarma@users.noreply.github.com>
2022-08-29 23:34:42 +0530

    Add files via upload

---
+++ meraki_qs7.py
@@ -0,0 +1,10 @@
+#meraki.q7
+def fun ():
+   reverse_list = ["Z", "A", "A", "B", "E", "M", "A", "R", "D"]
+   i=-1
+   a=[]
+   while i>=-len(reverse_list):
+     a.append(reverse_list[i])
+     i=i-1
+   print(a)
+fun ()
