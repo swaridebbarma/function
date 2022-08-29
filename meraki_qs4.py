c2132df15367bd49e1ca157c03748569bd300d09
swaridebbarma <98303710+swaridebbarma@users.noreply.github.com>
2022-08-29 23:36:26 +0530

    Add files via upload

---
+++ meraki_qs4.py
@@ -0,0 +1,11 @@
+#meraki.q4
+def fun ():
+  numbers = [3, 5, 7, 34, 2, 89, 2, 5]
+  i=0
+  max=0
+  while i<len(numbers):
+      if numbers[i]> max:
+          max=numbers[i]
+      i+=1
+  print(max)
+fun ()
