176558e2f305c9c7d6122fe19ec20dd0bab3cc85
swaridebbarma <98303710+swaridebbarma@users.noreply.github.com>
2022-08-29 23:20:39 +0530

    Add files via upload

---
+++ meraki.py
@@ -0,0 +1,34 @@
+#def max ():
+ #   numbers_list = [1, 2, 3, 4, 5, 6, 7, 10, -2]
+  #  i=0
+   # m=0
+    #while i<len(numbers_list):
+     #   if numbers_list[i]>m:
+      #      m=numbers_list[i]
+       # i+=1
+    #print (m)
+#max()
+
+def fun ():
+    a=[1,2,3,4,5,6]
+    i=0
+    c=0
+    while i<len(a):
+        c=c+1
+        i+=1
+    print(c)
+fun()
+    
+
+
+
+def max ():
+ numbers_list = [1, 2, 3, 4, 5, 6, 7, 10, -2]
+ i=0
+ m=0
+ while i<len(numbers_list):
+     if number_list[i]>m:
+         m=number_list[i]
+     i+=1
+print (m)
+max()
