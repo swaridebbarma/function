d5d6c6e391ba832abfbbe94816817fb8b1041f2e
swaridebbarma <98303710+swaridebbarma@users.noreply.github.com>
2022-08-29 23:12:34 +0530

    Add files via upload

---
+++ more_qs4.py
@@ -0,0 +1,12 @@
+def check_max_num(a,b,c):
+    if b<a>c:
+        print("mx:",a)
+    if a<b>c:
+        print("mx:",b)
+    if a<c>b:
+        print("mx:",c)
+a=int(input("enter no:"))
+b=int(input("enter no:"))
+c=int(input("enter no:"))
+check_max_num(a,b,c)
+#more_qs4
