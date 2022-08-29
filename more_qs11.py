c8333c55102b94ec17585d5a0075b54988b51e0c
swaridebbarma <98303710+swaridebbarma@users.noreply.github.com>
2022-08-29 23:20:06 +0530

    Add files via upload

---
+++ more_qs11.py
@@ -0,0 +1,18 @@
+def break_into_words():
+    sentence = ["NavGurukul is an alternative to higher education reducing the barriers of current formal education system"]
+    i=0
+    while i<len(sentence):
+        a=sentence[i]
+        b=a.split()
+        i+=1
+    print(b)             
+break_into_words()
+
+
+words = "navgurukul is great"
+i = 0
+while i < len(words):
+    a=(words[i])
+    print(a)
+    i+=1
+#more_qs11
